1c8f15749412ffdebd7133f4ce05db2f110c9bd6
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2022-02-27 01:42:52 -0500

    Modified setup script and added instructions.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # .setup
+
 My automated setup for new machines. 
+
+``` shell
+git clone https://github.com/U-DON/.setup.git ~/.setup \
+  && cd $_ \
+  && ./setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -2,7 +2,34 @@
 
 set -e
 
+bootstrap_linux() {
+  echo "Linux support TBD..."
+  exit 1
+}
+
+bootstrap_mac() {
+  sudo --validate
+
+  xcode-select --install || echo "Skipped command line developer tools installation."
+  /usr/sbin/softwareupdate --install-rosetta
+
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+}
+
+bootstrap_unsupported() {
+  echo "Unsupported OS!"
+  exit 1
+}
+
+case "$(uname -s)" in
+   Linux)  bootstrap_linux ;;
+  Darwin)  bootstrap_mac ;;
+       *)  bootstrap_unsupported ;;
+esac
+
 python3 -m venv .venv
 . .venv/bin/activate
-pip install ansible
-ansible-pull -U "https://github.com/U-DON/.setup" main.yml
+pip install --upgrade pip ansible
+
+ansible-playbook -vvvv main.yml
