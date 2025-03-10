5f6af001927f0de4e44f256764519f988e727b00
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2025-03-10 04:01:23 -0400

    Minor edit for print statement.

--- setup.sh
+++ setup.sh
@@ -5,26 +5,19 @@ set -e
 bootstrap_linux() {
   echo "Setting up bootstrap for Linux machine..."
   echo "Installing Ansible..."
-
   sudo apt update
   sudo apt install --yes software-properties-common
   sudo add-apt-repository --yes --update ppa:ansible/ansible
   sudo apt install --yes ansible
-
   echo "Installed Ansible!"
 }
 
 bootstrap_mac() {
   sudo --validate
-
-  xcode-select --install || echo "Skipped command line developer tools installation."
-
+  xcode-select --install || echo "Skipped installation for command line tools."
   /usr/sbin/softwareupdate --install-rosetta
-
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
   eval "$(/opt/homebrew/bin/brew shellenv)"
-
   brew install ansible
 }
 
@@ -34,9 +27,9 @@ bootstrap_unsupported() {
 }
 
 case "$(uname -s)" in
-   Linux)  bootstrap_linux ;;
-  Darwin)  bootstrap_mac ;;
-       *)  bootstrap_unsupported ;;
+   Linux) bootstrap_linux ;;
+  Darwin) bootstrap_mac ;;
+       *) bootstrap_unsupported ;;
 esac
 
 ansible-playbook -vvvv main.yml
