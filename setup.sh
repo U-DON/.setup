ca12e15e7c7e1fa9c04ace7cb8be05c8f80438c6
Eugene Doan <1067024+U-DON@users.noreply.github.com>
2025-03-11 01:11:56 -0400

    Added potentially missing dependency for bootstrap. More output on progress.

--- setup.sh
+++ setup.sh
@@ -3,22 +3,46 @@
 set -e
 
 bootstrap_linux() {
-  echo "Setting up bootstrap for Linux machine..."
-  echo "Installing Ansible..."
+  echo "Setting up bootstrap for Linux..."
+
   sudo apt update
-  sudo apt install --yes software-properties-common
+
+  echo "Installing dependencies for add-apt-repository..."
+  sudo apt install --yes \
+    python3-launchpadlib \
+    software-properties-common
+
+  echo "Adding apt repository for Ansible..."
   sudo add-apt-repository --yes --update ppa:ansible/ansible
+
+  echo "Installing Ansible..."
   sudo apt install --yes ansible
-  echo "Installed Ansible!"
+
+  echo "Finished bootstrap for Linux!"
 }
 
 bootstrap_mac() {
+  echo "Setting up bootstrap for macOS..."
   sudo --validate
+
+  echo "Installing command line tools..."
   xcode-select --install || echo "Skipped installation for command line tools."
-  /usr/sbin/softwareupdate --install-rosetta --agree-to-license
-  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  echo "Installing Rosetta..."
+  /usr/sbin/softwareupdate \
+    --install-rosetta \
+    --agree-to-license
+
+  echo "Installing Homebrew..."
+  NONINTERACTIVE=1 /bin/bash -c \
+    "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
   eval "$(/opt/homebrew/bin/brew shellenv)"
+
+  echo "Installing Ansible..."
   brew install ansible
+
+  echo "Finished bootstrap for macOS!"
 }
 
 bootstrap_unsupported() {
@@ -32,4 +56,5 @@ case "$(uname -s)" in
        *) bootstrap_unsupported ;;
 esac
 
+echo "Running Ansible playbook..."
 ansible-playbook -vvvv main.yml
